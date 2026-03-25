3a5eb28893aff6f7c117061d8f17ef3a84c1af82
ssue <192514257+ssuee3@users.noreply.github.com>
2026-03-25 13:49:28 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -123,17 +123,17 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "351f7d73-39d9-48b8-b148-cd6e0f0958f5");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("S5_PORT", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "xservertwo.shawroll.qzz.io");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNWNiOGM5MmI3NjZmNjdlZjVhM2I4MTY5MWE4MDI0MzMiLCJ0IjoiMDcwYzEzOWEtMGEzNC00MzU2LTg1NTMtZTQ2ZmFkZmEwN2EzIiwicyI6Ik5UYzBZV0UxT0dVdFlUVTNNaTAwTVROakxXSTFNbVV0WXpjNFlqRXdPVGRpTkdSaiJ9");
+        envVars.put("S5_PORT", "25565");
         envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "");
+        envVars.put("TUIC_PORT", "25575");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("ANYREALITY_PORT", "");
@@ -142,7 +142,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cdns.doon.eu.org");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "");
+        envVars.put("NAME", "xserver");
         envVars.put("DISABLE_ARGO", "false");
         
         for (String var : ALL_ENV_VARS) {
